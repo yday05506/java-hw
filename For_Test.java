b77aeba2c61ba109d845191970e4eea5def10d1d
yday05506 <85325266+yday05506@users.noreply.github.com>
2021-06-04 11:08:22 +0900

    대문자 A부터 Z까지 출력

---
+++ For_Test.java
@@ -0,0 +1,9 @@
+public class For_Test {
+	public static void main( String args[ ] ) {
+		int i;
+		for(int i=1; i <=10; i+=4) {
+			System.out.println(i);	//1 7
+		}
+		System.out.println(i);	//13
+	}
+}
