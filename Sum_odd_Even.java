01615b60ac01aef5814e484eb769d4856552ee30
yday05506 <85325266+yday05506@users.noreply.github.com>
2021-06-04 11:11:03 +0900

    입력한 값의 홀수와 짝수를 따로 합하기

---
+++ Sum_odd_Even.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Sum_odd_Even {
+	public static void main( String args[ ] ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("마지막 수 입력 : ");
+		int su = sc.nextInt( );
+		//홀수합
+		//짝수합
