ca02de6dfb1b9edfe8084e1b240f62cc7625a147
양다연 <85325266+yday05506@users.noreply.github.com>
2021-06-14 11:41:35 +0900

    [연습, for] 1부터 5까지의 값을 거꾸로 입력하기
    1 2 3 4 5
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ BaekJimin_yes.java
@@ -0,0 +1,9 @@
+public class BaekJimin_yes {
+	public static void main( String args[ ] ) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 6-i; j++)
+				System.out.print(j + "\t");
+			System.out.println( );
+		}
+	}
+}
