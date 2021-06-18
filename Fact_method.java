592b54786b3cd4d5f06e96e61bfe23df8902b927
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-18 10:45:52 +0900

    [for] 입력한 값의 누적곱 구하기

---
+++ Fact_method.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fact_method {
+	public static void main( String args[ ] ) {
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		int sum = 1;
+		System.out.print("╪Ж ют╥б : ");
+		int n = sc.nextInt( );
+		System.out.print("n! = ");
+
+		for (i = 1; i <= n; i++) {
+			sum *= i;
+			if (i==1) System.out.print("1");
+			else System.out.print("*" + i);
+		}
+		System.out.print(" = " + sum );
+	}
+}
