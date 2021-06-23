a811aba8a12d330876d3725985ee7b59363f38ac
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-23 12:09:15 +0900

    [for] 369 게임처럼 3, 6, 9가 들어간 곳은 X 처리

--- For_Test1.java
+++ For_Test1.java
@@ -1,8 +1,11 @@
 public class For_Test1 {
 	public static void main( String args[ ] ) {
-	
-		for (char a = 'A'; a <= 'Z'; a++) {
-			System.out.println(a);
+		for (int i = 1; i <= 100; i++) {
+				if ( i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+					System.out.print("X ");
+				else 
+					System.out.print(i+ " ");
+		if ( i % 10 == 0) System.out.println("\t");
 		}
 	}
 }
