e537683de8750b45ceab5394854df8e05d523dda
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-23 12:08:32 +0900

    [for] 소수 판별

--- Prime.java
+++ Prime.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 public class Prime {
 	public static void main( String args[ ] ) {
 		Scanner sc = new Scanner(System.in);
-		boolean b = false;
+		int num = 0;
 		System.out.print("수 입력 : ");
 		int a = sc.nextInt( );
 		System.out.print(a + "은(는) ");
 
-		for (int i = 2; i <= a; i++) {
+		for (int i = 1; i <= a; i++) {
 			if (a % i == 0)
-				b = true;
-		break;
+				num++;
 		}
-		if (b) 
-			System.out.print("소수가 아닙니다.");
-		else
+		if (num == 2) 
 			System.out.print("소수입니다.");
+		else
+			System.out.print("소수가 아닙니다.");
 	}
 }
