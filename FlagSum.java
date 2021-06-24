7d6e52ac27a6a170bee7a0f5dfac242abe61fe3c
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-25 00:23:07 +0900

    [for] 값을 입력하고 값까지의 홀수는 더하고 짝수는 빼기

--- FlagSum.java
+++ FlagSum.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 public class FlagSum {
 	public static void main( String args[ ] ) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner (System.in);
+		int i;
+		int sum = 1;
 		int n = sc.nextInt( );
-		int sum = 0;
-
-		for (int i = 1; i <= n; i++) {
+		for (i = 1; i <= n; i++) {
 			if (i == 1) {
+				sum += 1;
 				System.out.print("1");
 			} else {
-				if ( i % 2 == 0) {
+				if (i % 2 == 0) {
 					sum -= i;
-					System.out.print(-i);
+					System.out.print("-" + i);
 				} else {
 					sum += i;
 					System.out.print("+" + i);
 				}
 			}
 		}
-		System.out.print("=" + (sum + 1) );
+		System.out.print("= " + sum);
 	}
 }
