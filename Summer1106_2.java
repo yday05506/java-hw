8f9069ed7db0aa976c9bd1b3d2ce81c5d2b197e1
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-08-14 00:00:04 +0900

    [scanner] 이름, 학번, 나이를 입력

---
+++ Summer1106_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Summer1106_2 {
+	public static void main (String args [ ] ) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("이름을 입력하시오 : ");
+		String name = sc.nextLine( );
+		System.out.print("학번을 입력하시오 : ");
+		int s_num = sc.nextInt( );
+		System.out.print("나이를 입력하시오 : ");
+		int age = sc.nextInt( );
+	}
+}
