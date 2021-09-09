dc1cfbf0a7d2e009f3d78b42bcd4be3fd148e107
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-09 23:49:48 +0900

    [array] 1부터 100까지의 수 중 홀수는 더하고 짝수는 빼기

--- Array_sum.java
+++ Array_sum.java
@@ -1,28 +1,59 @@
+package Array;
 
 public class Array_sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = new int[100];
-		int i = 0;
-		a[0] = 0;
 		int sum = 0;
-		for (i = 1; i <= a.length; i++)
-		{
-			if (i % 2 == 0) {
-				sum -= i;
-				System.out.print("-"+i);
+		int b=1;
+		for (int i = 0; i < a.length; i++) {
+			a[i]=(i+1);
+			if(a[i] % 2 == 0) {
+				b=-1;
+				a[i]*=b;
+				System.out.print(a[i]);
 			}
-			else if (i == 1) {
-				sum+=1;
+			else if (i==0) {
 				System.out.print("1");
 			}
+			
 			else {
-				sum += i;
-				System.out.print("+"+i);
+				b=1;
+				a[i]*=b;
+				System.out.print("+"+a[i]);
 			}
+			sum += a[i];
 		}
-		System.out.println(" = "+sum);
+		System.out.print(" = "+sum);
 	}
 
 }
+
+//package Array;
+//
+//public class Array_sum {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int[] a = new int[100];
+//		int sum = 0;
+//		for (int i = 1; i <= a.length; i++)
+//		{
+//			if (i % 2 == 0) {
+//				sum -= i;
+//				System.out.print("-"+i);
+//			}
+//			else if (i == 1) {
+//				sum+=1;
+//				System.out.print("1");
+//			}
+//			else {
+//				sum += i;
+//				System.out.print("+"+i);
+//			}
+//		}
+//		System.out.println(" = "+sum);
+//	}
+//
+//}
