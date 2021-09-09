acd848ca0689047cf2fb33f85113dd45f55d7085
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-09 23:50:06 +0900

    [array] 1부터 100까지 모두 더하기

--- ForEx2.java
+++ ForEx2.java
@@ -1,19 +1,17 @@
-
+package Array;
 public class ForEx2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = new int[100];
-		a[0] = 0;
 		int sum = 0;
 		
-		for(int i = 1; i <= a.length; i++)
+		for(int i = 0; i < a.length; i++)
 		{
-			sum += i;
-			if (i == 1)
-				System.out.print("1");
-			else
-				System.out.print("+"+i);
+			a[i] = (i+1);
+			if (i == 0) System.out.print("1"); 
+			else System.out.print("+"+a[i]);
+			sum += a[i];
 		}
 		System.out.print(" = " + sum);
 
