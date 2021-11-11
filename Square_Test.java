ca7c5f9e8ba25e69571f0062049f3c2c93f498e1
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-11-11 15:17:36 +0900

    [class] 사각형의 넓이와 둘레 구하기

--- Square_Test.java
+++ Square_Test.java
@@ -9,8 +9,12 @@ class Square {
 		width = wi;
 	}
 	
-	int area() {
+	int round() {
 		return (2*height) + (2*width);
+	}
+	
+	int area() {
+		return height * width;
 	}
 }
 
@@ -19,6 +23,7 @@ public class Square_Test {
 	public static void main(String[] args) {
 		Square S1 = new Square((int)(Math.random()*10+1), (int)(Math.random()*10+1));
 		
+		System.out.println("세로의 길이가 " + S1.width + "고, 가로의 길이가 " + S1.height + "인 사각형의  둘레 : " + S1.round());
 		System.out.println("세로의 길이가 " + S1.width + "고, 가로의 길이가 " + S1.height + "인 사각형의  넓이 : " + S1.area());
 	}
 
